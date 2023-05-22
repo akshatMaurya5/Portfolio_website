ca781266bc9d76658e0748efeda9d4ede4c3e42e
Akshat <mauryaakshat5@gmail.com>
2023-05-22 10:35:03 +0530

    .

---
+++ src/data/services.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { MdWeb } from "react-icons/md";
+
+import { FaHandHoldingHeart } from "react-icons/fa";
+
+import { VscCode } from "react-icons/vsc";
+
+export default [
+  {
+    title: "Leetcode",
+    icon: <MdWeb className="w-full h-full" />,
+    link: "https://leetcode.com/heisenberg_5/",
+
+    description:
+      // "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi rerum quia harum eaque, quae laboriosam quisquam distinctio.",
+      "1000+ probems solved at LEETCODE.",
+  },
+  {
+    title: "Codeforces",
+    icon: <FaHandHoldingHeart className="w-full h-full" />,
+    link: "https://codeforces.com/profile/alchemist_5",
+    description:
+      // "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi rerum quia harum eaque, quae laboriosam quisquam distinctio.",
+      "Specialist at CODEFORCES.",
+  },
+  ,
+  {
+    title: "Codechef",
+    icon: <VscCode className="w-full h-full" />,
+    link: "https://www.codechef.com/users/tirederit",
+
+    description:
+      // "Lorem ipsum dolor sit amet consetetur adipisicing elit. Quasi rerum quia harum eaque, quae laboriosam quisquam distinctio.",
+      "3 🌟 at CODECHEF.",
+  },
+];
